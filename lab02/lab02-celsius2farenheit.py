1e0a3a7e0a834532472e0d79b39763e9ad021a97
Suyapa Judith Rodriguez Perez (24150) <suyapa.rodriguez@est.zamorano.edu>
2022-09-08 19:37:55 -0600

    Ultima versión lab02

---
+++ lab02/lab02-celsius2farenheit.py
@@ -0,0 +1,5 @@
+#Tendencias e Innovación en Tecnología Agrícola (TEA)
+
+celsius = input("Ingrese la temperatura en grados celsius: ")
+farenheit = (float(celsius) * 9/5) + 32
+print("La temperatura en grados Farenheit es: ", farenheit)
